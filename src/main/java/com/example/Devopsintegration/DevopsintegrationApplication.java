a4cb3abbaec3fc372cc40f2948cf5025c0c7fa3c
Yejun Lao <waynelao7@gmail.com>
2023-06-06 17:34:49 -0400

    first commit

---
+++ src/main/java/com/example/Devopsintegration/DevopsintegrationApplication.java
@@ -0,0 +1,21 @@
+package com.example.Devopsintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DevopsintegrationApplication {
+
+	@GetMapping
+	public String message() {
+		return "welcome to javatechie";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsintegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Devopsintegration/DevopsintegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Devopsintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsintegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
